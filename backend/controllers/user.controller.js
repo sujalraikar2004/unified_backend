fa6730f43a92a989610dd5272da206fe7d129da3
sujalraikar2004 <sujalraikar84@gmail.com>
2025-09-03 17:33:45 +0530

    jjjjkk

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -10,7 +10,7 @@ const sendTokenResponse = (user, statusCode, res) => {
 
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === 'development',
     sameSite: 'strict',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
